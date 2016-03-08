5bec9345aaf16e24683e0af51ff775564d05a2f6
Davey Wood <daveyw1234@gmail.com>
2016-03-07 20:56:11 -0500

    add basic post body deserialization

--- turquoise/ParameterBinding/DefaultBinder.cs
+++ turquoise/ParameterBinding/DefaultBinder.cs
@@ -1,23 +1,70 @@
 using Turquoise;
 using System.Linq;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Turquoise.ParameterBinding
 {
     public class DefaultBinder : ParameterBinder
     {
         //TODO: other primitive types
-        private static readonly Type[] _supportedTypes = new Type[]{ typeof(string), typeof(int) };
+        private static readonly HashSet<Type> _primitiveTypes = new HashSet<Type>{
+            typeof(int),
+            typeof(double),
+            typeof(float),
+            typeof(long),
+            typeof(short),
+            typeof(int?),
+            typeof(double?),
+            typeof(float?),
+            typeof(long?),
+            typeof(short?),
+            typeof(uint),
+            typeof(ulong),
+            typeof(ushort),
+            typeof(uint?),
+            typeof(ulong?),
+            typeof(ushort?),
+            typeof(string),
+            typeof(bool),
+            typeof(bool?),
+            typeof(Guid),
+            typeof(Guid?),
+            typeof(decimal),
+            typeof(decimal?)
+        };
         
         public override bool SupportsType(Type parameterType)
         {
-            return _supportedTypes.Contains(parameterType);
+            return true;
         }
+        
+        // TODO: I'm assuming this is safe to share
+        private static readonly JsonSerializer _serializer = new JsonSerializer();
         
         public override object Bind(Request request, string parameterName, Type parameterType)
         {
+            if (_primitiveTypes.Contains(parameterType))
+            {
+                return BindPrimitive(request, parameterName, parameterType);
+            }
+            
+            // TODO: content negotiation, assume JSON for this pass
+            var requestBody = request.RequestBody;
+            using (var streamReader = new StreamReader(requestBody))
+            {
+                using (var jsonReader = new JsonTextReader(streamReader))
+                {
+                    return _serializer.Deserialize(jsonReader, parameterType);
+                }
+            }
+            
+        }
+        public object BindPrimitive(Request request, string parameterName, Type parameterType)
+        {
             //TODO: what if there are multiple values here?
-            //var stringValue = request.QueryString.ContainsKey(parameterName) ? request.QueryString[parameterName][0] : null;
             string stringValue = null;
             
             if (request.RouteTokens.ContainsKey(parameterName))
@@ -42,6 +89,8 @@ namespace Turquoise.ParameterBinding
                 //I don't like returning 4xx here without calling the action, because it prevents further validations
                 return 0;
             }
+            
+            //TODO: tons of types, I would like to think of a clever way to not have to handcode all of them
             
             return stringValue;
         }
